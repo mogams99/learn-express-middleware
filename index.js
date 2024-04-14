0f89520a3542e6aba978fcbd27888572d7a597f1
Mochammad Gamal <mogams1811@gmail.com>
2024-04-14 17:17:50 +0700

    chore: :construction: create simple middleware

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const morgan = require('morgan');
 
 // Config
 app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    console.log('Init middleware 1');
+    next();
+    console.log('Init middleware 1 after next');
+});
+app.use((req, res, next) => {
+    console.log('Init middleware 2');
+    next();
+});
 
 // Routing
 app.get('/', (req, res) => {
     res.send({
         message: 'Hello World!'
     });
+
 });
 app.get('/products', (req, res) => {
     res.send({
